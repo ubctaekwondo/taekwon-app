fddb9f3c7522d6ef1caad83b54b114e61b1ecf79
michelleykim <yeojin011016@gmail.com>
2024-11-11 16:36:48 -0800

    super minor cleanup

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,13 +1,10 @@
 import { Stack } from "expo-router";
 
 export default function AuthLayout() {
-  return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{ headerTitle: "Sign in Screen", headerLeft: () => <></> }}
-      />
-      <Stack.Screen name="signup" options={{ headerTitle: "Sign Up Screen" }} />
-    </Stack>
-  );
+	return (
+		<Stack>
+			<Stack.Screen name="index" options={{ headerTitle: "Sign in Screen" }} />
+			<Stack.Screen name="signup" options={{ headerTitle: "Sign Up Screen" }} />
+		</Stack>
+	);
 }
