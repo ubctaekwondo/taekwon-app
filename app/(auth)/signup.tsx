879bd673eb7df7f10c1b3d062508a41d4d114513
michelleykim <yeojin011016@gmail.com>
2024-11-12 18:35:31 -0800

    minor changes

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -1,15 +1,13 @@
 import { useState } from "react";
 import { Link } from "expo-router";
 import {
-  Image,
-  Text,
-  View,
-  StyleSheet,
-  TextInput,
-  TouchableOpacity,
-  Alert,
-  ScrollView,
-  TextInputProps,
+	Image,
+	Text,
+	View,
+	StyleSheet,
+	TextInput,
+	ScrollView,
+	TextInputProps,
 } from "react-native";
 import GlobalStyles from "@/constants/GlobalStyles";
 import { moderateScale, scale } from "react-native-size-matters";
@@ -17,150 +15,150 @@ import { moderateScale, scale } from "react-native-size-matters";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 interface CustomInputProps extends TextInputProps {
-  icon: string;
+	icon: string;
 }
 
 const CustomInput: React.FC<CustomInputProps> = ({
-  icon,
-  placeholder,
-  value,
-  onChangeText,
-  keyboardType,
-  secureTextEntry,
+	icon,
+	placeholder,
+	value,
+	onChangeText,
+	keyboardType,
+	secureTextEntry,
 }) => {
-  return (
-    <View style={GlobalStyles.inputContainer}>
-      <Icon
-        name={icon}
-        size={moderateScale(20)}
-        color="#838383"
-        style={GlobalStyles.icon}
-      />
-      <TextInput
-        style={GlobalStyles.input}
-        placeholder={placeholder}
-        placeholderTextColor="#838383"
-        value={value}
-        onChangeText={onChangeText}
-        keyboardType={keyboardType}
-        secureTextEntry={secureTextEntry}
-      />
-    </View>
-  );
+	return (
+		<View style={GlobalStyles.inputContainer}>
+			<Icon
+				name={icon}
+				size={moderateScale(20)}
+				color="#838383"
+				style={GlobalStyles.icon}
+			/>
+			<TextInput
+				style={GlobalStyles.input}
+				placeholder={placeholder}
+				placeholderTextColor="#838383"
+				value={value}
+				onChangeText={onChangeText}
+				keyboardType={keyboardType}
+				secureTextEntry={secureTextEntry}
+			/>
+		</View>
+	);
 };
 
 export default function SignUp() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-  // const handleSignUp = async () => {
-  //   if (firstName && lastName && email && password.length >= 8) {
-  //     try {
-  //       await createUserWithEmailAndPassword(auth, email, password);
-  //       Alert.alert("Success", "Account created successfully!");
-  //     } catch (error) {
-  //       Alert.alert("Error", error.message);
-  //     }
-  //   } else {
-  //     Alert.alert("Error", "Please fill out all fields and ensure the password is at least 8 characters.");
-  //   }
-  // };
+	// const handleSignUp = async () => {
+	//   if (firstName && lastName && email && password.length >= 8) {
+	//     try {
+	//       await createUserWithEmailAndPassword(auth, email, password);
+	//       Alert.alert("Success", "Account created successfully!");
+	//     } catch (error) {
+	//       Alert.alert("Error", error.message);
+	//     }
+	//   } else {
+	//     Alert.alert("Error", "Please fill out all fields and ensure the password is at least 8 characters.");
+	//   }
+	// };
 
-  return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-      <View style={GlobalStyles.container}>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "flex-end",
-            padding: scale(0),
-            width: "100%",
-          }}
-        >
-          <Image
-            source={require("@/assets/images/TKDLogo.png")}
-            style={styles.logo}
-            resizeMode="contain"
-          ></Image>
-        </View>
+	return (
+		<ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+			<View style={GlobalStyles.container}>
+				<View
+					style={{
+						flexDirection: "row",
+						justifyContent: "flex-end",
+						padding: scale(0),
+						width: "100%",
+					}}
+				>
+					<Image
+						source={require("@/assets/images/TKDLogo.png")}
+						style={styles.logo}
+						resizeMode="contain"
+					></Image>
+				</View>
 
-        <Text style={[GlobalStyles.title, { marginBottom: -10 }]}>
-          Welcome!
-        </Text>
-        <Text style={[GlobalStyles.text, { marginBottom: 40 }]}>
-          Enter your details below to create account:
-        </Text>
+				<Text style={[GlobalStyles.title, { marginBottom: -10 }]}>
+					Welcome!
+				</Text>
+				<Text style={[GlobalStyles.text, { marginBottom: 40 }]}>
+					Enter your details below to create account:
+				</Text>
 
-        <Text style={styles.boldtext}>First name</Text>
-        <CustomInput
-          icon="user"
-          placeholder="Enter your first name"
-          value={firstName}
-          onChangeText={setFirstName}
-        />
+				<Text style={styles.boldtext}>First name</Text>
+				<CustomInput
+					icon="user"
+					placeholder="Enter your first name"
+					value={firstName}
+					onChangeText={setFirstName}
+				/>
 
-        <Text style={styles.boldtext}>Last name</Text>
-        <CustomInput
-          icon="user"
-          placeholder="Enter your last name"
-          value={lastName}
-          onChangeText={setLastName}
-        />
+				<Text style={styles.boldtext}>Last name</Text>
+				<CustomInput
+					icon="user"
+					placeholder="Enter your last name"
+					value={lastName}
+					onChangeText={setLastName}
+				/>
 
-        <Text style={styles.boldtext}>Email</Text>
-        <CustomInput
-          icon="envelope"
-          placeholder="Enter your email"
-          value={email}
-          onChangeText={setEmail}
-          keyboardType="email-address"
-        />
+				<Text style={styles.boldtext}>Email</Text>
+				<CustomInput
+					icon="envelope"
+					placeholder="Enter your email"
+					value={email}
+					onChangeText={setEmail}
+					keyboardType="email-address"
+				/>
 
-        <Text style={styles.boldtext}>Password</Text>
-        <CustomInput
-          icon="lock"
-          placeholder="Enter your password"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-        />
+				<Text style={styles.boldtext}>Password</Text>
+				<CustomInput
+					icon="lock"
+					placeholder="Enter your password"
+					value={password}
+					onChangeText={setPassword}
+					secureTextEntry
+				/>
 
-        <Text style={styles.graytext}>Minimum 8 characters</Text>
+				<Text style={styles.graytext}>Minimum 8 characters</Text>
 
-        {/* <TouchableOpacity onPress={handleSignUp} style={GlobalStyles.button}>
+				{/* <TouchableOpacity onPress={handleSignUp} style={GlobalStyles.button}>
         <Text style={GlobalStyles.buttonText}>SIGN UP</Text>
       </TouchableOpacity> */}
 
-        <Link href={"/today"} style={GlobalStyles.signInButton}>
-          <Text style={GlobalStyles.signInButtonText}>SIGN UP</Text>
-        </Link>
+				<Link href={"/today"} style={GlobalStyles.signInButton}>
+					<Text style={GlobalStyles.signInButtonText}>SIGN UP</Text>
+				</Link>
 
-        <Text style={[GlobalStyles.text, { textAlign: "center" }]}>
-          Already have an account?{" "}
-          <Link href={"/"} style={GlobalStyles.link}>
-            Sign in.
-          </Link>
-        </Text>
-      </View>
-    </ScrollView>
-  );
+				<Text style={[GlobalStyles.text, { textAlign: "center" }]}>
+					Already have an account?{" "}
+					<Link href={"/"} style={GlobalStyles.link}>
+						Sign in.
+					</Link>
+				</Text>
+			</View>
+		</ScrollView>
+	);
 }
 
 const styles = StyleSheet.create({
-  logo: {
-    width: scale(100),
-    height: scale(100),
-  },
-  graytext: {
-    textAlign: "right",
-    color: "#838383",
-    alignSelf: "flex-end",
-    fontSize: moderateScale(12),
-  },
-  boldtext: {
-    ...GlobalStyles.text,
-    fontWeight: "bold",
-  },
+	logo: {
+		width: scale(64),
+		height: scale(64),
+	},
+	graytext: {
+		textAlign: "right",
+		color: "#838383",
+		alignSelf: "flex-end",
+		fontSize: moderateScale(12),
+	},
+	boldtext: {
+		...GlobalStyles.text,
+		fontWeight: "bold",
+	},
 });
--- constants/GlobalStyles.tsx
+++ constants/GlobalStyles.tsx
@@ -3,66 +3,66 @@ import { scale, moderateScale } from "react-native-size-matters";
 import colors from "./TKDColors";
 
 const GlobalStyles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: scale(50),
-    paddingVertical: scale(50),
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: colors.tkdbeige,
-  },
-  text: {
-    fontSize: moderateScale(14),
-    color: "#373737",
-    textAlign: "left",
-    width: "100%",
-  },
-  title: {
-    fontSize: moderateScale(48),
-    fontWeight: "bold",
-    color: "#373737",
-    alignSelf: "flex-start",
-    textAlign: "left",
-  },
-  link: {
-    fontSize: moderateScale(14),
-    textDecorationLine: "underline",
-    color: "#373737",
-  },
-  inputContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    borderWidth: scale(1),
-    borderColor: "#373737",
-    borderRadius: scale(25),
-    backgroundColor: "#f0eee6",
-    paddingHorizontal: scale(10),
-    marginTop: scale(4),
-    marginBottom: scale(12),
-    width: "100%",
-  },
-  icon: {
-    marginRight: scale(10),
-  },
-  input: {
-    flex: 1,
-    fontSize: moderateScale(14),
-    color: "#373737",
-    paddingVertical: scale(8),
-    backgroundColor: "transparent",
-  },
-  signInButton: {
-    backgroundColor: "#D9534F", // Button color (red in this case)
-    paddingVertical: scale(15), // Vertical padding for height
-    paddingHorizontal: scale(30), // Horizontal padding for width
-    borderRadius: scale(10), // Rounded corners
-    alignItems: "center", // Center text inside the button
-    marginVertical: scale(30), // Space above and below the button
-  },
-  signInButtonText: {
-    color: "#fff", // White text color
-    fontSize: moderateScale(16), // Font size
-  },
+	container: {
+		flex: 1,
+		paddingHorizontal: scale(28),
+		paddingVertical: scale(48),
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: colors.tkdbeige,
+	},
+	text: {
+		fontSize: moderateScale(12),
+		color: "#373737",
+		textAlign: "left",
+		width: "100%",
+	},
+	title: {
+		fontSize: moderateScale(48),
+		fontWeight: "bold",
+		color: "#373737",
+		alignSelf: "flex-start",
+		textAlign: "left",
+	},
+	link: {
+		fontSize: moderateScale(12),
+		textDecorationLine: "underline",
+		color: "#373737",
+	},
+	inputContainer: {
+		flexDirection: "row",
+		alignItems: "center",
+		borderWidth: scale(1),
+		borderColor: "#373737",
+		borderRadius: scale(24),
+		backgroundColor: "#f0eee6",
+		paddingHorizontal: scale(12),
+		marginTop: scale(4),
+		marginBottom: scale(8),
+		width: "100%",
+	},
+	icon: {
+		marginRight: scale(8),
+	},
+	input: {
+		flex: 1,
+		fontSize: moderateScale(12),
+		color: "#373737",
+		paddingVertical: scale(8),
+		backgroundColor: "transparent",
+	},
+	signInButton: {
+		backgroundColor: "#D9534F", // Button color (red in this case)
+		paddingVertical: scale(16), // Vertical padding for height
+		paddingHorizontal: scale(32), // Horizontal padding for width
+		borderRadius: scale(12), // Rounded corners
+		alignItems: "center", // Center text inside the button
+		marginVertical: scale(28), // Space above and below the button
+	},
+	signInButtonText: {
+		color: "#fff", // White text color
+		fontSize: moderateScale(12), // Font size
+	},
 });
 
 export default GlobalStyles;
